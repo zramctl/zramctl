970b7211611ff43e6c1fa6e39a46af43eabeb733
1yusof <zerowatts@tuta.io>
2024-08-28 22:05:13 -0400

    refactor: :fire: nevermind, I can't make pr's to webring.zip

--- src/components/webring.tsx
+++
@@ -1,9 +0,0 @@
-export function Webring() {
-  return (
-    <section className="flex gap-1 underline text-purple-300 text-xl">
-      <a href="https://webring.zip/prev/zram.sh">←</a>
-      <a href="https://webring.zip">webring.zip</a>
-      <a href="https://webring.zip/next/zram.sh">→</a>
-    </section>
-  );
-} // https://webring.zip
